8592b2455c04bd583272ed33e3eb682e6c629937
voidmahipal <mahipal7131@gmail.com>
2026-07-30 23:16:36 +0530

    Added the webpack to the project. The files are now bundled and deployable.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve("dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8080,
+    open: true,
+    watchFiles: ["./src/template.html"],
+   },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./src/template.html"}),
+  ],
+  module: {
+    rules: [
+            {
+              test: /\.css$/,
+              use: ["style-loader", "css-loader"],
+            },
+            {
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: "asset/resource",
+            }
+    ],
+  },
+};
